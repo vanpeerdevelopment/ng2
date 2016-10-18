f6d457ede2a7a612ebe3d0c20762f8c821c39ecf
vanpeerdevelopment <dieter@vanpeerdevelopment.be>
2016-10-18 20:47:52 +0200

    tour of heroes - routing

--- tour-of-heroes/app/hero.service.ts
+++ tour-of-heroes/app/hero.service.ts
@@ -1,17 +1,22 @@
-import { Injectable } from '@angular/core';
+import { Injectable }   from '@angular/core';
 
-import {Hero} from "./hero";
-import {HEROES} from "./mock-heroes";
+import {Hero}           from "./hero";
+import {HEROES}         from "./mock-heroes";
 
 @Injectable()
 export class HeroService {
+
+    getHero(id:number):Promise<Hero> {
+        return this.getHeroes()
+            .then(heroes => heroes.find(hero => hero.id === id));
+    }
 
     getHeroes(): Promise<Hero[]> {
         return Promise.resolve(HEROES);
     }
 
     getHeroesSlowly(): Promise<Hero[]> {
-        return new Promise<Hero[]>(resolve => setTimeout(resolve, 2000)) // delay 2 seconds
+        return new Promise<Hero[]>(resolve => setTimeout(resolve, 2000))
             .then(() => this.getHeroes());
     }
 }
---
+++ tour-of-heroes/app/heroes.component.ts
@@ -0,0 +1,40 @@
+import {Component, OnInit}  from '@angular/core';
+import {Router}             from '@angular/router';
+
+import {HeroService}        from './hero.service';
+import {Hero}               from './hero';
+
+@Component({
+    moduleId: module.id,
+    selector: 'my-heroes',
+    templateUrl: 'heroes.component.html',
+    styleUrls: ['heroes.component.css']
+})
+export class HeroesComponent implements OnInit{
+    heroes:Hero[];
+    selectedHero:Hero;
+
+    constructor(
+        private router:Router,
+        private heroService:HeroService){
+    }
+
+    ngOnInit():void {
+        this.getHeroes();
+    }
+
+    getHeroes(): void {
+        this.heroService
+            .getHeroes()
+            .then(heroes => this.heroes = heroes);
+    }
+
+    onSelect(hero:Hero):void {
+        this.selectedHero = hero;
+    }
+
+    gotoDetail():void {
+        let link = ['/detail', this.selectedHero.id];
+        this.router.navigate(link);
+    }
+}
